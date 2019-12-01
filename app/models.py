65e392aee66ba9237d74bc71da6b9a6c5e000730
jkingz <jfkrule@gmail.com>
2019-12-01 23:53:59 +0800

    Created new class method verify hashed password

--- app/models.py
+++ app/models.py
@@ -39,3 +39,7 @@ class UserModel(db.Model):
     @staticmethod
     def generate_hash(password):
         return sha256.hash(password)
+
+    @staticmethod
+    def verify_hash(password, hash):
+        return sha256.verify(password, hash)
